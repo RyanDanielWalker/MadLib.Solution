5b531ea032bbf87791bf05fda93e7fe58c778c5d
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-10 13:33:44 -0700

    Add functionality for linking multiple html pages
     Co-authored-by: Min Chang <minchangmhc@gmail.com>

--- Madlib/Controllers/HomeController.cs
+++ Madlib/Controllers/HomeController.cs
@@ -6,6 +6,12 @@ namespace Madlib.Controllers
   public class HomeController : Controller
   {
     [Route("/")]
+    public ActionResult Splash()
+    {
+      return View();
+    }
+
+    [Route("/form")]
     public ActionResult Form()
     {
       return View();
@@ -13,6 +19,19 @@ namespace Madlib.Controllers
 
     [Route("/madlib")]
     public ActionResult Madlib(string firstWord, string secondWord, string thirdWord, string fourthWord, string fifthWord, string sixthWord, string seventhWord)
+    {
+      MadlibInputs myMadlibVariable = new MadlibInputs();
+      myMadlibVariable.First = firstWord;
+      myMadlibVariable.Second = secondWord;
+      myMadlibVariable.Third = thirdWord;
+      myMadlibVariable.Fourth = fourthWord;
+      myMadlibVariable.Fifth = fifthWord;
+      myMadlibVariable.Sixth = sixthWord;
+      myMadlibVariable.Seventh = seventhWord;
+      return View(myMadlibVariable);
+    }
+    [Route("/madlibTwo")]
+    public ActionResult MadlibTwo(string firstWord, string secondWord, string thirdWord, string fourthWord, string fifthWord, string sixthWord, string seventhWord)
     {
       MadlibInputs myMadlibVariable = new MadlibInputs();
       myMadlibVariable.First = firstWord;
